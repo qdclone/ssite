edf48909d0a25edc8258275b60c98db40c3401fe
qdsang <qdsang@gmail.com>
2014-09-08 17:58:08 +0800

    中秋节快乐~

--- manage/index.js
+++ manage/index.js
@@ -3,10 +3,12 @@
 var meta = require('../package.json'),
     express = require('express'),
     compress = require('compression'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
     compileSass = require('express-compile-sass'),
     path = require('path'),
-    app = module.exports = express(),
-    router = require('./router')
+    app = module.exports = express()
 ;
 
 process.on('uncaughtException', function (err) {
@@ -27,23 +29,37 @@ app.use(compileSass(root), {
     logToConsole: false // If true, will log to console.error on errors
 });
 
-app.use(router({'index': '/'}));
+// load controllers
+require('./lib/boot')(app, { verbose: !module.parent });
 
 app.use(express.static(__dirname + '/public'));
 //app.use(express.static(__dirname));
 //app.use('/public', express.static(app.get('root') + './public'));
 
-app.use(function(err, req, res, next){
-  // log it
-  if (!module.parent) console.error(err.stack);
+// session support
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'some secret here'
+}));
 
-  // error page
-  res.status(500).end('500');//.render('5xx');
+// parse request bodies (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow overriding methods in query (?_method=put)
+app.use(methodOverride('_method'));
+
+app.use(function(err, req, res, next){
+    // log it
+    if (!module.parent) console.error(err.stack);
+
+    // error page
+    res.status(500).end('500');//.render('5xx');
 });
 
 // assume 404 since no middleware responded
 app.use(function(req, res, next){
-  res.status(404).end(req.originalUrl);//.render('404', { url: req.originalUrl });
+    res.status(404).end(req.originalUrl);//.render('404', { url: req.originalUrl });
 });
 
 if (require.main === module) {
